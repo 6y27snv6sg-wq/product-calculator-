cc6e6492c8c86b3e7e573bc68e03cea73fd818b5
6y27snv6sg-wq <6y27snv6sg@privaterelay.appleid.com>
2026-08-30 22:15:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-import os
-import sys
-
-# تثبيت openpyxl تلقائياً في حال عدم وجودها
-try:
-    import openpyxl
-except ImportError:
-    import subprocess
-
-    subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", "openpyxl"]
-    )
-    import openpyxl
-
 import datetime
 import tempfile
+import openpyxl
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Font, PatternFill
 import pandas as pd
